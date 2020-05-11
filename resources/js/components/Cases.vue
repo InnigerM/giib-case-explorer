914cc61416410c1c1b5be629a502448844cd6293
InnigerM <marco.inniger@gmail.com>
2020-05-11 11:24:36 +0200

    Updated case card styling

--- resources/js/components/Cases.vue
+++ resources/js/components/Cases.vue
@@ -2,10 +2,10 @@
     <div>
         <h1 class="page-title">View all Cases here</h1>
         <div class="primary flex flex-wrap">
-            <div class="case-item" v-for="post in posts" @click="showPost(post.id)">
+            <div class="case-item" v-for="post in posts">
                 <div class="card-image" v-bind:style="{ backgroundImage: 'url(' + post.image_link + ')' }"></div>
                 <h5 v-text="post.title" class="mb-4 text-2xl"></h5>
-                <p class="text-green-900 hover:underline">See more</p>
+                <button class="btn btn-primary" @click="showPost(post.id)">See more</button>
             </div>
         </div>
     </div>
