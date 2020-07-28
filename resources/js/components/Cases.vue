9801e142c9319ff7d93d234f2930d5641c46d6d0
InnigerM <marco.inniger@gmail.com>
2020-07-28 16:43:33 +0200

    Fixed misclick case error

--- resources/js/components/Cases.vue
+++ resources/js/components/Cases.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="primary flex flex-wrap justify-center">
-        <div class="case-item shadow-inner-one shadow-inner-two bg-gray-100 flex-none" v-for="post in articles">
-            <div class="card-image" @click="showPost(post.id)"
+        <div class="case-item shadow-inner-one shadow-inner-two bg-gray-100 flex-none" @click="showPost(post.id)" v-for="post in articles">
+            <div class="card-image"
                  v-bind:style="{ backgroundImage: 'url(' + post.image_link + ')' }"></div>
             <h5 v-text="post.title" class="mt-2 mb-4 text-2xl"></h5>
         </div>
