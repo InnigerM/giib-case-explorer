2f6c3c84d08770c605a1bc3d02be49edaa23df03
InnigerM <marco.inniger@gmail.com>
2020-07-30 10:00:47 +0200

    Refactored paragraphs to make them dynamically visible

---
+++ resources/js/components/Paragraph.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3 v-text="paragraph.title"
+            @click="toggleCollapse"
+            class="text-gray-900 pl-8 mb-4 text-xl bg-no-repeat bg-contain"
+            :style="{ backgroundImage: 'url(' + ('../../images/arrow_right.png') + ')' }"></h3>
+        <p v-show="visible" v-text="paragraph.content" class="pb-8"></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Paragraph",
+
+        props: {
+            paragraph: Object,
+            visible: Boolean,
+        },
+
+        methods: {
+            toggleCollapse(){
+                this.visible = !this.visible;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/TagOverview.vue
+++ resources/js/components/TagOverview.vue
@@ -1,13 +1,23 @@
 <template>
-
-    <div class="bg-gray-100 rounded-2xl pb-8 shadow-box-one shadow-box-two">
-        <h1 class="page-title">All {{tag.name}} Cases</h1>
-        <Cases :articles="tag.articles"></Cases>
+    <div class="relative">
+        <button
+            class="nav-aligned"
+            @click="goToPage"
+            :style="{ backgroundImage: 'url(' + ('../../images/back.png') + ')' }">
+        </button>
+        <div class="py-12 px-4 bg-gray-100 rounded-2xl pb-8 shadow-box-one shadow-box-two">
+            <div class="box-gradient">
+                <h1 class="ml-0 pt-0 page-title">Alle "{{tag.name}}" Cases</h1>
+                <p v-text="tag.description" class="text-gray-900"></p>
+            </div>
+            <Cases :articles="tag.articles"></Cases>
+        </div>
     </div>
 </template>
 
 <script>
     import Cases from "./Cases";
+
     export default {
         name: "TagOverview",
         components: {Cases},
@@ -16,6 +26,7 @@
                 tag: {
                     id: 0,
                     name: '',
+                    description: '',
                     articles: []
                 }
             }
@@ -24,6 +35,9 @@
         methods: {
             showPost(id) {
                 this.$router.push({path: `/cases/${id}`});
+            },
+            goToPage() {
+                this.$router.go(-1);
             }
         },
 
@@ -35,6 +49,7 @@
             axios.get(`http://giibTest.test/api/tags/${this.$route.params.id}`)
                 .then(response => {
                     this.tag.id = response.data.id;
+                    this.tag.description = response.data.description;
                     this.tag.name = response.data.name;
                 })
         }
