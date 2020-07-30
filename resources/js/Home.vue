3bc495f9f1e7d21ee13194bcbd509d3d9d267190
InnigerM <marco.inniger@gmail.com>
2020-07-30 18:58:51 +0200

    Redesigned Homepage

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -1,23 +1,31 @@
 <template>
-    <div class="relative">
-        <button
-            class="nav-aligned"
-            @click="goToPage('')"
-            :style="{ backgroundImage: 'url(' + ('../../images/back.png') + ')' }">
-        </button>
+    <div class="relative h-screen">
+        <goHome></goHome>
         <div class="py-12 px-4 bg-gray-100 rounded-2xl pb-8 shadow-box-one shadow-box-two">
             <div class="box-gradient">
                 <h1 class="page-title">Geschäftsinnovation im Bauwesen</h1>
-                <p>Hier sind alle Informationen rund um die Website beschrieben</p>
-            </div>
-            <div class="box-gradient flex">
-                <div class="w-1/2 box-gradient m-2">
-                    <h2 class="text-2xl text-gray-900">Lass dich inspirieren</h2>
-                    <Explore></Explore>
-                </div>
-                <div class="w-1/2 box-gradient m-2">
-                    <h2 class="text-2xl text-gray-900">Suchen und Filtern</h2>
-                    <CaseOverview></CaseOverview>
+                <p class="mb-8">Hier sind alle Informationen rund um die Website beschrieben</p>
+                <div class="grid grid-cols-2">
+                    <div
+                        class="row-span-1 box-gradient m-2 bg-center bg-cover bg-no-repeat h-128 hover:font-semibold relative"
+                        @click="goToPage('explore')"
+                        :style="{ backgroundImage: 'url(' + require('../images/House_Detailed_Day.png') + ')' }">
+                        <div class="p-4 absolute left-0 bottom-0 bg-secondary-700 rounded-b-2xl">
+                            <h2 class="text-2xl text-white">Entdecken</h2>
+                            <p class="text-white">Du weisst (noch) nicht genau, wo dich dein Weg hinführen soll? Lass
+                                dich hier inspirieren</p>
+                        </div>
+                    </div>
+                    <div
+                        class="row-span-1 box-gradient m-2 bg-center bg-cover bg-no-repeat h-128 hover:font-semibold relative"
+                        @click="goToPage('cases')"
+                        :style="{ backgroundImage: 'url(' + ('../../images/House_Detailed_Day_Cover.png') + ')' }">
+                        <div class="p-4 absolute left-0 bottom-0 bg-secondary-700 rounded-b-2xl">
+                            <h2 class="text-2xl text-white">Alles Ansehen</h2>
+                            <p class="text-white">Du warst schon mal hier? Oder du weisst genau wonach du suchst?
+                                Dann bist du hier richtig aufgehoben</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -25,12 +33,12 @@
 </template>
 
 <script>
-import Explore from "./components/Explore";
-import CaseOverview from "./components/CaseOverview";
+import GoHome from "./GoHome";
+
 export default {
     name: "Home.vue",
 
-    components: {CaseOverview, Explore},
+    components: {GoHome},
 
     methods: {
         goToPage(link) {
@@ -40,6 +48,3 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
